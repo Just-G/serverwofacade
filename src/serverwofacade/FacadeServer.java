23586950ebaa68f6d7691c5b6e0e2770ade8ea8a
JustG <53385979+Just-G@users.noreply.github.com>
2021-09-05 17:57:09 +0700

    FacadeServer by 62050240

---
+++ src/serverwofacade/FacadeServer.java
@@ -0,0 +1,35 @@
+package serverwofacade;
+
+public class FacadeServer {
+    private static ScheduleServer scheduleServer = new ScheduleServer();
+    private static FacadeServer facade = null;
+
+    FacadeServer() {
+    }
+
+    public static FacadeServer getCommand() {
+        if (facade == null) {
+            facade = new FacadeServer();
+        }
+        return facade;
+    }
+
+    public static void Start() {
+        scheduleServer.startBooting();
+	    scheduleServer.readSystemConfigFile();
+	    scheduleServer.init();
+	    scheduleServer.initializeContext();
+	    scheduleServer.initializeListeners();
+	    scheduleServer.createSystemObjects();
+    }
+
+    public static void Stop(){
+        scheduleServer.releaseProcesses();
+	    scheduleServer.destory();
+	    scheduleServer.destroySystemObjects();
+	    scheduleServer.destoryListeners();
+	    scheduleServer.destoryContext();
+	    scheduleServer.shutdown();
+    }
+
+}
